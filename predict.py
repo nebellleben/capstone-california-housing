ee4918f36200d0855179419ed9610b54b275202d
Kelvin Chan <kelvinchan@Kelvins-MacBook-Air.local>
2026-01-01 17:51:25 +0800

    Deployment

--- predict.py
+++ predict.py
@@ -6,22 +6,13 @@ import pandas as pd
 
 app = FastAPI(title="California Housing Prediction Model")
 
-with open('baseline_model.pkl', 'rb') as f:
-    baseline_model = pickle.load(f)
-
-with open('lasso_model.pkl', 'rb') as f:
-    lasso_model = pickle.load(f)
-
-with open('ridge_model.pkl', 'rb') as f:
-    ridge_model = pickle.load(f)
-
-with open('nn_model.pkl', 'rb') as f:
-    nn_model = pickle.load(f)
+with open('model.pkl', 'rb') as f:
+    dv, baseline_model, lasso_model, ridge_model, nn_model = pickle.load(f)
 
 def predict_single(housing_data):
     df = pd.DataFrame([housing_data])
     df_dict = df.to_dict(orient='records')
-    X = nn_model.transform(df_dict)
+    X = dv.transform(df_dict)
 
     y_pred_baseline = float(baseline_model.predict(X)[0]) 
     y_pred_lasso = float(lasso_model.predict(X)[0]) 
